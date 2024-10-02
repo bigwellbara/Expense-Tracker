805da1ccca9461c18ea7ff2fd3b12e1c13eb3db7
Bigwell Baramasimbe <bbigwel@gmail.com>
2024-10-02 09:54:07 +0200

    added income model

---
+++ Expense Tracker/Models/Income.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Expense_Tracker.Models
+{
+    public class Income
+    {
+        [BsonId]  // Map _id to IncomeId
+        [BsonRepresentation(BsonType.String)]  
+        public Guid IncomeId { get; set; } = Guid.NewGuid();
+
+        [BsonElement("Amount")]  
+        public decimal Amount { get; set; }
+
+        [BsonElement("Source")]  
+        public string Source { get; set; }
+
+        [BsonElement("Description")]  
+        public string Description { get; set; } = "";
+
+        [BsonElement("IncomeDate")]  // Date when the income transaction occurred
+        public DateTime IncomeDate { get; set; }
+
+        [BsonElement("UserId")]  // Foreign key reference to User
+        [BsonRepresentation(BsonType.String)]  // Store as a string (you can use ObjectId if preferred)
+        public Guid UserId { get; set; }
+
+        [BsonElement("CategoryId")]  // Foreign key reference to Category
+        [BsonRepresentation(BsonType.String)]  // Store as a string (you can use ObjectId if preferred)
+        public Guid CategoryId { get; set; }
+
+        [BsonElement("CreatedDate")]  
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
---
+++ Expense Tracker/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Expense_Tracker.Models
+{
+    public class User
+    {
+        [BsonId]  // Map _id to UserId
+        [BsonRepresentation(BsonType.String)]  
+        public Guid UserId { get; set; } = Guid.NewGuid();
+
+        [BsonElement("FirstName")]  
+        public string FirstName { get; set; }
+
+        [BsonElement("LastName")]  
+        public string LastName { get; set; }
+
+        [BsonElement("Email")] 
+        [BsonRepresentation(BsonType.String)]
+        public string Email { get; set; }
+
+        [BsonElement("PasswordHash")]  
+        public string PasswordHash { get; set; }
+
+        [BsonElement("CreatedDate")]  
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        [BsonElement("UpdatedDate")]  
+        public DateTime UpdatedDate { get; set; } = DateTime.Now;
+    }
+}
