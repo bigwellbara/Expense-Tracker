cf6239ed88ede3b120c34dd2d2bc2e062fb66e4f
Bigwell Baramasimbe <bbigwel@gmail.com>
2024-10-02 11:20:09 +0200

    added recurring expense

---
+++ Expense Tracker/Models/Budget.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Expense_Tracker.Models
+{
+    public class Budget
+    {
+        [BsonId]  // Map _id to BudgetId
+        [BsonRepresentation(BsonType.String)]  // Store the ID as a string (optional)
+        public Guid BudgetId { get; set; } = Guid.NewGuid();
+
+        [BsonElement("Amount")]  
+        public decimal Amount { get; set; }
+
+        [BsonElement("StartDate")]  // Start date of the budget period
+        public DateTime StartDate { get; set; }
+
+        [BsonElement("EndDate")]  // End date of the budget period
+        public DateTime EndDate { get; set; }
+
+        [BsonElement("UserId")]  // Foreign key reference to User
+        [BsonRepresentation(BsonType.String)] 
+        public Guid UserId { get; set; }
+
+        [BsonElement("CategoryId")]  // Foreign key reference to Category
+        [BsonRepresentation(BsonType.String)]  // Store as a string (you can use ObjectId if preferred)
+        public Guid CategoryId { get; set; }
+
+        [BsonElement("CreatedDate")]  // Automatically set the creation date
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
---
+++ Expense Tracker/Models/RecurringExpense.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Expense_Tracker.Models
+{
+    public class RecurringExpense
+    {
+        [BsonId]  // Map _id to RecurringExpenseId
+        [BsonRepresentation(BsonType.String)]  // Store the ID as a string (optional)
+        public Guid RecurringExpenseId { get; set; } = Guid.NewGuid();
+
+        [BsonElement("Amount")]  
+        public decimal Amount { get; set; }
+
+        [BsonElement("Title")] 
+        public string Title { get; set; }
+
+        [BsonElement("Description")]  
+        public string Description { get; set; } = "";
+
+        [BsonElement("StartDate")]  // Start date for the recurring expense
+        public DateTime StartDate { get; set; }
+
+        [BsonElement("Frequency")]  // Frequency of the recurring expense (e.g., Monthly, Weekly)
+        public string Frequency { get; set; }
+
+        [BsonElement("UserId")]  // Foreign key reference to User
+        [BsonRepresentation(BsonType.String)]  // Store as a string (you can use ObjectId if preferred)
+        public Guid UserId { get; set; }
+
+        [BsonElement("CategoryId")]  // Foreign key reference to Category
+        [BsonRepresentation(BsonType.String)]  // Store as a string (you can use ObjectId if preferred)
+        public Guid CategoryId { get; set; }
+
+        [BsonElement("CreatedDate")]  // Automatically set the creation date
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
