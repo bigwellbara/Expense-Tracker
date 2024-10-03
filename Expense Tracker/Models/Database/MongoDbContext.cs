10ea2dd24a59630a6ac1ce823ce91121bc8f8f53
Bigwell Baramasimbe <bbigwel@gmail.com>
2024-10-03 09:20:32 +0200

    updated the mongodb context

--- Expense Tracker/Models/Database/MongoDbContext.cs
+++ Expense Tracker/Models/Database/MongoDbContext.cs
@@ -15,5 +15,11 @@ namespace Expense_Tracker.Models.Database
 
         public IMongoCollection<Transaction> Transactions => _database.GetCollection<Transaction>("Transactions");
         public IMongoCollection<Category> Categories => _database.GetCollection<Category>("Categories");
+        public IMongoCollection<User> Users => _database.GetCollection<User>("Users");
+        public IMongoCollection<Expense> Expenses => _database.GetCollection<Expense>("Expenses");
+        public IMongoCollection<Income> Incomes => _database.GetCollection<Income>("Incomes");
+        public IMongoCollection<PaymentMethod> PaymentMethods => _database.GetCollection<PaymentMethod>("PaymentMethods");
+        public IMongoCollection<RecurringExpense> RecurringExpenses => _database.GetCollection<RecurringExpense>("RecurringExpenses");
+        public IMongoCollection<Budget> Budgets => _database.GetCollection<Budget>("Budgets");
     }
 }
